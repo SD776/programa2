f324790e1bab8d070706341ae38d53d207edef64
SDGuillen <duartesofia131@gmail.com>
2023-03-25 23:02:02 -0400

    cambios

--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
-define('BASE_URL', 'http://localhost/programa/');
+define('BASE_URL', 'http://localhost/dsaimports/');
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
 define('DBNAME', 'sistema-mod');
 define('CHARSET', '');
-define('TITLE', 'DSA Import');
+define('TITLE', 'DSA Imports');
 define('MONEDA', '$');
 define('HOST_SMTP', 'smtp.gmail.com');
 define('USER_SMTP', 'pcorreo940@gmail.com');
